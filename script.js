207fd1237d71992ab47da42e3192f3114a7a6bb2
jrporto <joao6roberto@gmail.com>
2023-12-22 17:38:26 -0300

    Starting to implement overall logic and matchup config, still with query issues

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const scsBtn = document.querySelector("#scissors");
+const rckBtn = document.querySelector('div>button#rock');
+const prpBtn = document.querySelector("#paper");
+
+rckBtn.addEventListener("click", game("PEDRA"));
+prpBtn.addEventListener("click", game("PAPEL"));
+scsBtn.addEventListener("click", game("TESOURA"));
+
 function getComputerChoice() {
     const options = ["PEDRA","PAPEL","TESOURA"];
     let index;
@@ -8,45 +16,107 @@ function getComputerChoice() {
 function playMatch(playerChoice, computerChoice) {
     switch (true) {
         case (playerChoice == computerChoice):
-            return ("EMPATE");
+            return [("EMPATE"),"E"];
         case (playerChoice == "PAPEL" && computerChoice == "TESOURA"):
         case (playerChoice == "TESOURA" && computerChoice == "PEDRA"):
         case (playerChoice == "PEDRA" && computerChoice == "PAPEL"):
-            return (`DERROTA! Infelizmente, a sua escolha ${playerChoice} perdeu para ${computerChoice}`);
+            return [(`DERROTA! Infelizmente, a sua escolha ${playerChoice} perdeu para ${computerChoice}`),"V"];
         default:
-            return (`VITORIA! Felizmente, a sua escolha ${playerChoice} ganhou de ${computerChoice}`);
+            return [(`VITORIA! Felizmente, a sua escolha ${playerChoice} ganhou de ${computerChoice}`),"D"];
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let npcScore = 0;
+let playerScore;
+let computerScore;
 
+function game(playerChoice) {
     let playerChoice;
-    let computerChoice;
-    computerChoice = getComputerChoice();
+    let computerChoice = getComputerChoice();
 
-    let msgRound;
-    while (npcScore < 5 && playerScore < 5) {
-        playerChoice = prompt("Escolha sua jogada:").toUpperCase();
-        msgRound = playMatch(playerChoice, computerChoice);
-        switch (msgRound[0]) {
-            case "V":
-                playerScore++;
-                break;
-            case "D":
-                npcScore++;
-                break;
-            default:
-                break;
-        };
-        console.log(msgRound);
-    };
-    if (playerScore > npcScore) {
-        console.log("Você venceu a partida!");
+    let msgRound = playMatch(playerChoice,computerChoice);
+    
+    switch (msgRound[2]) {
+        case "V":
+            playerScore ++; 
+            updateScore("Player");
+            break;
+        case "D":
+            computerScore ++;
+            updateScore("Computer");
+            break;
+        default:
+            break;
     }
-    else {
-        console.log("Você perdeu a partida!");
+    if (playerScore > 5) {
+        endMatch("Player");
     }
-    console.log(`O placar final foi de ${playerScore} X ${npcScore}`);
+    if (computerScore > 5) {
+        endMatch("Computer");
+    }
 }
+
+const playerDisplay = document.querySelector("#playerScore");
+const computerDisplay = document.querySelector("pcScore");
+
+function updateScore(winner) {
+    switch (winner) {
+        case "Player":
+            playerDisplay.textContent = `Player: ${playerScore}`;
+            break;
+        case "Computer":
+            computerScore.textContent = `Computer: ${computerScore}`;
+            break;
+        default:
+            playerDisplay.textContent = "Player: 0";
+            computerScore.textContent = "Computer: 0";
+            break;
+    } 
+}
+
+function endMatch(winner) {
+    switch (winner) {
+        case "Player":
+
+            break;
+        case "Computer":
+
+            break;
+        default:
+            break;
+    } 
+    playerScore = 0;
+    computerScore = 0;
+    updateScore("reset");
+}
+
+//function game() {
+//    let playerScore = 0;
+//    let npcScore = 0;
+//
+//    let playerChoice;
+//    let computerChoice;
+//    computerChoice = getComputerChoice();
+//
+//    let msgRound;
+//    while (npcScore < 5 && playerScore < 5) {
+//        msgRound = playMatch(playerChoice, computerChoice);
+//        switch (msgRound[0]) {
+//            case "V":
+//                playerScore++;
+//                break;
+//            case "D":
+//                npcScore++;
+//                break;
+//            default:
+//                break;
+//        };
+//        console.log(msgRound);
+//    };
+//    if (playerScore > npcScore) {
+//        console.log("Você venceu a partida!");
+//    }
+//    else {
+//        console.log("Você perdeu a partida!");
+//    }
+//    console.log(`O placar final foi de ${playerScore} X ${npcScore}`);
+//}
